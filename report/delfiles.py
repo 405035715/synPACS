f20e146eac97c3336b0f6ada3859749d3cb30deb
Administrator <405035715@hotmail.com>
2016-09-14 14:34:09 +0800

    a

--- report/delfiles.py
+++ report/delfiles.py
@@ -9,14 +9,15 @@ if __name__ == '__main__':
     for str in childpathlist:
         temppath = path + os.sep +str
         if os.path.isdir(temppath):  #判断是否目录
-            print ('目录:%s'%temppath)
+            print('目录:%s'%temppath)
         elif os.path.isfile(temppath):  #判断是否文件
             timestamp = os.path.getmtime(temppath) #文件的修改时间的时间戳
             date = datetime.datetime.fromtimestamp(timestamp)
-            ##判断时间
-
-            print (date.strftime('%Y-%m-%d %H:%M:%S'))
-            print ('文件:%s'%temppath)
-    #print (os.listdir(path))
+            ##判断时间:早于三天前
+            if date < (datetime.datetime.now() - datetime.timedelta(days=3)):
+                pass
+            print(date.strftime('%Y-%m-%d %H:%M:%S'))
+            print('文件:%s'% temppath)
+    # print (os.listdir(path))
 
     pass
