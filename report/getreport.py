876cb18f4cbc23e7457aa64e3ebf917cf972d11f
zyh <405035715@qq.com>
2016-10-16 06:51:58 +0800

    update:getreport.py ,sendreport.py

--- report/getreport.py
+++ report/getreport.py
@@ -1,48 +1,43 @@
-import pyodbc
 import datetime
 import cx_Oracle
 import sys
 import socket
 import socketserver
 import _thread
-import schedule
 import time
 import re
 import hashlib
 import urllib.request
 import urllib.parse
+import os
+import mysql.connector
 
-# 连接oracle数据库
-# 读取视图，按每次1000条记录读取，
-# 读取后，把记录写到web服务器。
+#1.未上传01-01的除ct,cr以外的影像,.1月 14/24/29/30 2月5,10,24  3月1,2,3
+#20150218之前的压缩时jpeg压缩,没有转jpeg2000
+#已上传 :20150101/ 20150308
 
-# 当前查询日期
-STUDYDONEDATE = datetime.datetime.strptime('2009-10-18 00:00:00', '%Y-%m-%d %H:%M:%S')
+'''
+配置项目
+'''
+
+# 查询开始时间
+STUDYDONEDATE = datetime.datetime.strptime('2015-03-31 00:00:00', '%Y-%m-%d %H:%M:%S')
+#查询结束时间
+STUDYENDDATE = datetime.datetime.strptime('2015-04-10 00:00:00', '%Y-%m-%d %H:%M:%S')
+
+#获取病人列表
+# 获取报告sql语句
+
+
 STUDYLIST = []  # 检查的列表
 STUDYINDEX = 0  # 发送的检查索引
 SOCKETCONTENT = ''  # socket接收到的内容
 
-
-
-# 发送报告内容
-def sendSocket(content):
-    HOST = '192.168.1.25'  # The remote host
-    PORT = 6700  # The same port as used by the server
-    # uid = r'studyuid=\\192.9.216.23\pacsimage2\NearLine\201603\20160316\X354954\A2614136'
-    # uid = r'studyuid=end'
-    uid = r'%s' % content
-    s = socket.socket(socket.AF_INET,
-                      socket.SOCK_DGRAM)  # socket.SOCK_DGRAM -for udp   socket.SOCK_STREAM- for tcp
-    s.connect((HOST, PORT))
-    s.sendall(bytes(uid, encoding='utf8', errors='字符串错误'))
-    data = s.recv(1024)
-    s.close()
-    print('Received', repr(data))
-
 # 查询数据库
 def selSql(selStr):
     sqlresult = []
-    connection = cx_Oracle.Connection("system/Jdimage123@192.168.2.112/ypacs")
+    print(selStr)
+    connection = cx_Oracle.Connection("radinfo/pacs.jd@192.9.216.21/radinfo")
     cursor = connection.cursor()
     try:
         cursor.execute(selStr)
@@ -55,72 +50,182 @@ def selSql(selStr):
         if not row:
             break
         sqlresult.append(list(row))
-        # print(row)
-    # print(sum)
+        if sum == 111:
+            pass
+        print(row)
     connection.close()
     return sqlresult
 
+# 更新数据库
+def updatemysql(sqlstr, values):
+    # update_old_salary = (
+    #     "UPDATE salaries SET to_date = %s "
+    #     "WHERE emp_no = %s AND from_date = %s")
+    cnx = mysql.connector.connect(user='root', password='',
+                                  host='127.0.0.1',
+                                  database='ypacs')
+    cursor = cnx.cursor()
+    cursor.execute(sqlstr, values)
+    # Make sure data is committed to the database
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+#计算年龄
+def calculate_age(born,today):
+    try:
+        birthday = born.replace(year=today.year)
+    except ValueError:
+        # raised when birth date is February 29
+        # and the current year is not a leap year
+        birthday = born.replace(year=today.year, day=born.day - 1)
+    age = ''
+    if birthday > today:
+        age = str(today.year - born.year - 1)
+    else:
+        age = str(today.year - born.year)
+    # if age == '0':   #小于1岁,按月
+    #     age = str(today.month -born.month)+'M'
+    # if age == '0M':
+    #     age = str(today.day-born.day)+'D'
+    return  age
+
+# 发送报告内容
+def sendSocket(content):
+    HOST = '127.0.0.1'  # The remote host
+    PORT = 6700  # The same port as used by the server
+    # uid = r'studyuid=\\192.9.216.23\pacsimage2\NearLine\201603\20160316\X354954\A2614136'
+    # uid = r'studyuid=end'
+    uid = r'%s' % content
+    s = socket.socket(socket.AF_INET,
+                      socket.SOCK_DGRAM)  # socket.SOCK_DGRAM -for udp   socket.SOCK_STREAM- for tcp
+    s.connect((HOST, PORT))
+    s.sendall(bytes(uid, encoding='utf8', errors='字符串错误'))
+    #data = s.recv(1024)
+    s.close()
+    time.sleep(0.01)
+    #print('Received', repr(data))
+
+
+
 
 # 获取病人列表
-def getpatientlist(STUDYDONEDATE):
-    selstr = 'select STUDIESINSTUID,ACCESSIONNUMBER  from vhis WHERE  REPORTSSTATUS = 100 and STUDYDONEDATE>=%s and STUDYDONEDATE< %s order by StudiesDoneTime' % (STUDYDONEDATE.strftime('%Y%m%d'), (STUDYDONEDATE + datetime.timedelta(hours=24)).strftime('%Y%m%d'))
+def getpatientlist():
+    global  STUDYDONEDATE
+    selstr = 'select STUDIESINSTUID,ACCESSIONNUMBER  from VHIS_JDYX WHERE  REPORTSSTATUS = 100 and STUDIESDONEDATE>=\'%s\' and STUDIESDONEDATE< \'%s\'  order by StudiesDoneTime' % (STUDYDONEDATE.strftime('%Y%m%d'), (STUDYDONEDATE + datetime.timedelta(hours=24)).strftime('%Y%m%d'))
+    #print(selstr)
     patientlistsql = selSql(selstr)
+    #print(selstr)
     return patientlistsql
 
 
 # 获取检查的所有image路径
 def getimages(accessionnumber):
-    selstr = 'SELECT IMAGESFILENAME FROM vpacsimages  where AccessionNumber= \'%s\' ' % accessionnumber
+    selstr = 'SELECT IMAGESFILENAME,SERIESMODALITY FROM vpacsimages  where AccessionNumber= \'%s\' ' % accessionnumber
+    #print (selstr)
     imagelistsql = selSql(selstr)
-    imagelist = []
-    for temp in imagelistsql:
-        imagelist.append(temp[0])
-    print(imagelist)
+    # imagelist = []
+    #
+    # for temp in imagelistsql:
+    #     imagelist.append(temp[0])
+    #print(imagelist)
+    return imagelistsql
 
-# 获取报告
-def getreport(STUDYDONEDATE):
-    selstr = 'select STUDIESINSTUID,STUDIESMODALITIES,PATIENTSALIAS,ACCESSIONNUMBER,STUDYDONEDATE,STUDIESDONETIME,RESULTSEXAMINEALIAS,REPORTSDOCTORALIAS,REPORTSEVIDENCES,REPORTSCONCLUSION,APPROVEDATE,APPROVETIME,RECORDSDOCTORALIAS  from vhis WHERE  REPORTSSTATUS = 100 and STUDYDONEDATE>=%s and STUDYDONEDATE< %s order by StudiesDoneTime' % (STUDYDONEDATE.strftime('%Y%m%d'), (STUDYDONEDATE + datetime.timedelta(hours=24)).strftime('%Y%m%d'))
+'''
+获取报告
+'''
+def getreport():
+    global  STUDYDONEDATE
+    selstr = 'select STUDIESINSTUID,STUDIESMODALITIES,PATIENTSALIAS,ACCESSIONNUMBER,STUDIESDONEDATE,STUDIESDONETIME,RESULTSEXAMINEALIAS,REPORTSDOCTORALIAS,REPORTSEVIDENCES,REPORTSCONCLUSION,APPROVEDATE,APPROVETIME,RECORDSDOCTORALIAS,PATIENTSSEX,PATIENTSDOB,PATIENTSID,REPORTSDATE,REPORTSTIME  from VHIS_JDYX WHERE  REPORTSSTATUS = 100 and STUDIESDONEDATE>=\'%s\' and STUDIESDONEDATE< \'%s\' order by StudiesDoneTime' % (STUDYDONEDATE.strftime('%Y%m%d'), (STUDYDONEDATE + datetime.timedelta(hours=24)).strftime('%Y%m%d'))
     reportlistsql = selSql(selstr)
     reportdic = {}
     for temp in reportlistsql:
         report = {}
         m1 = hashlib.md5()
         m1.update(temp[0].encode('utf_8'))
+        report = {'appKey': '', 'uuid': '', 'patientPhone': '', 'modality': '', 'hospitalName': '', 'pacsCode': '',
+                     'patientName': '', 'patientSex': '', 'patientAge': 0, 'typeName': '', 'clinicId': '',
+                     'applyDoctorName': '', 'officeName': '', 'patientId': '', 'studyDate': '', 'reportBody': '',
+                     'reportDoctorName': '', 'imageShow': '', 'diagnosis': '', 'reportDate': '', 'auditDoctorName': '',}
         report['uuid'] = m1.hexdigest() # md5 加密
-        report['modality'] = temp[1][1:-1]
+        if temp[1]:
+            if len(temp[1]) > 2:
+                report['modality'] = temp[1][1:-1]
+            else:
+                report['modality'] = temp[1]
         report['patientName'] = temp[2]
-        report['patientId'] = temp[3]
+        #report['ACCESSIONNUMBER'] = temp[3]
         report['studyDate'] = datetime.datetime.strptime(temp[4] + temp[5], '%Y%m%d%H%M%S').strftime(
             '%Y-%m-%d %H:%M:%S')
         report['reportBody'] = temp[6]
         report['reportDoctorName'] = temp[7]
         report['imageShow'] = temp[8]
         report['diagnosis'] = temp[9]
-        report['reportDate'] = datetime.datetime.strptime(temp[10] + temp[11], '%Y%m%d%H%M%S').strftime(
+        report['reportDate'] = datetime.datetime.strptime(temp[16] + temp[17], '%Y%m%d%H%M%S').strftime(
             '%Y-%m-%d %H:%M:%S')
         report['auditDoctorName'] = temp[12]
-
-        reportdic['clinicId'] = ''
-        reportdic['typeName'] = ''
-        reportdic['patientSex'] = ''
-        reportdic['patientPhone'] = ''
-        reportdic['hospitalName'] = '影像云诊断中心'
-        reportdic['pacsCode'] = 'jdimage'
-
-        # reportdic['applyDoctorName'] = '张**'
-
+        if temp[13]:
+            report['patientSex'] = temp[13].replace('F', '女').replace('M', '男')
+        else:
+            report['patientSex'] ='/'
+        report['patientAge'] = calculate_age(datetime.datetime.strptime(temp[14], '%Y%m%d'),datetime.datetime.strptime(report['reportDate'], '%Y-%m-%d %H:%M:%S'))
+        report['patientId'] = temp[3]
+        report['clinicId'] = '/'
+        report['typeName'] = '/'
+        report['officeName'] = '/'
+        report['patientPhone'] = '/'
+        report['hospitalName'] = '建德市第一人民医院'
+        report['pacsCode'] = 'JDRMYY'
+        report['applyDoctorName'] = '/'
+        report['auditDate']  = datetime.datetime.strptime(temp[10] + temp[11], '%Y%m%d%H%M%S').strftime(
+            '%Y-%m-%d %H:%M:%S')
         reportdic[report['uuid']] = report
     return reportdic
 
 # 发送一个study的image路径
-def sendimages(STUDYINDEX):
+def sendimages():
+    global STUDYINDEX, SOCKETCONTENT
     study = STUDYLIST[STUDYINDEX]
+    print('sendimages')
     imagelist = getimages(study[1])
+    studyimagesifull = 1   # 是否study的image有丢失0有丢失1没有丢失
+    studyimagesisexist = 0 # 是否study的有image   0没有1有
+    if len(imagelist) == 0:
+        print('没有影像')     # error
+        SOCKETCONTENT = study[0] + '=0'
+        return
+    extimages = []
     for image in imagelist:  # 逐一发送image路径
-        content = study[0] + '=' + image
+        if os.path.exists(image[0]):
+            studyimagesisexist = 1
+            if image[1] == 'SR' or image[1] =='REQUEST' or image[1] == 'SC' or image[1] == 'PR':   # 'SR''REGUEST'的最后发送
+                extimages.append(image)
+            else:
+                content = study[0] + '=' + image[0]
+                print(content)
+                sendSocket(content)
+        else:
+            studyimagesifull = 0
+    for extimage in extimages:
+        content = study[0] + '=' + extimage[0]
+        print(content)
         sendSocket(content)
-    contentend = study[0] + '=end'  # 一个stduy的路径全发送完成后，发送一个完成标识
-    sendSocket(contentend)
+    if studyimagesisexist == 1:
+        contentend = study[0] + '=end'  # 一个stduy的路径全发送完成后，发送一个完成标识
+        print(contentend)
+        sendSocket(contentend)
+    studyimagestatus = 0    # 0图像空1图像不全2图像全
+    if studyimagesifull == 1:
+        studyimagestatus =2
+    elif studyimagesisexist ==1:
+        studyimagestatus = 1
+    print(datetime.datetime.now())
+    # 记录状态到数据库
+    sqlstr = (
+        "UPDATE vhis_jdyx SET SENDIMAGES = %s "
+        "WHERE AccessionNumber = %s ")
+    values = (studyimagestatus, study[1])
+    updatemysql(sqlstr, values)
 
 # socket服务
 class MyUDPHandler(socketserver.BaseRequestHandler):
@@ -129,13 +234,13 @@ class MyUDPHandler(socketserver.BaseRequestHandler):
         data = self.request[0].strip()
         socket = self.request[1]
         # print("{} wrote:".format(self.client_address[0]))
-        # print(data)
+        print(data)
         SOCKETCONTENT = data.decode()
         socket.sendto(data.upper(), self.client_address)
 
 # socketserver 打开
 def startsocketserver():
-    HOST, PORT = "192.168.1.138", 6701
+    HOST, PORT = "127.0.0.1", 6701
     server = socketserver.UDPServer((HOST, PORT), MyUDPHandler)
     server.serve_forever()
 
@@ -145,20 +250,31 @@ def submitreport(reportdic):
     # 接口地址
     url = 'http://app.jdimage.cn/api/report/upload'
     reportdata = urllib.parse.urlencode(reportdic)
-    reportdata = reportdata.encode('utf-8')
-    with urllib.request.urlopen(url, reportdata) as f:
-        postResult = eval(
-            (f.read().decode('utf-8')).replace('true', 'True'))  # {"flag":true}{"msg":"有参数未传","flag":false}
-        if 'flag' in postResult.keys():
-            if True == postResult['flag']:
-                print(postResult['flag'])
-            else:  # 发送报告失败，保存到数据库
-                pass
-        print(f.read().decode('utf-8'))
+    if len(reportdic['reportBody']) < 500:
+        reportdata = reportdata.encode('utf-8')
+
+
+        with urllib.request.urlopen(url, reportdata) as f:
+            tmp = (f.read().decode('utf-8')).replace('true', 'True').replace('false', 'False')
+            postResult = eval(tmp)  # {"flag":true}{"msg":"有参数未传","flag":false}
+            print(postResult)
+            if 'flag' in postResult.keys():
+                if True == postResult['flag']:
+                    print(postResult['flag'])
+                    # 记录状态到数据库
+                    sqlstr = (
+                        "UPDATE vhis_jdyx SET SENDREPORT = %s  "
+                        "WHERE AccessionNumber = %s  ")
+                    values = ( 1, reportdic['patientId'])
+                    updatemysql(sqlstr, values)
+                else:  # 发送报告失败，保存到数据库
+                    pass
+            print(f.read().decode('utf-8'))
 
 
 
 if __name__ == "__main__":
+    os.environ['nls_lang'] = 'AMERICAN_AMERICA.AL32UTF8' #解决CX_ORACLE查询出错的编码问题,
     # print(reportlist)
     # 开启socket服务
     try:
@@ -166,32 +282,47 @@ if __name__ == "__main__":
     except:
         print( "Error: unable to start thread socketServer")
 
-    reportdic = getreport(STUDYDONEDATE)      # 获取一天的报告
-    STUDYLIST = getpatientlist(STUDYDONEDATE)  # 获取一条的检查
+    reportdic = getreport()        # 获取一天的报告
+    STUDYLIST = getpatientlist()   # 获取一条的检查
+    # print(STUDYLIST)
     if STUDYINDEX < len(STUDYLIST):
-         sendimages(STUDYINDEX)                 # 发送一个检查的所有图像
-    else:
-        STUDYDONEDATE = STUDYDONEDATE + datetime.timedelta(hours=24)  # 日期 +1天
-
+        sendimages()               # 发送一个检查的所有图像
+    #else:
+        # STUDYDONEDATE = STUDYDONEDATE + datetime.timedelta(hours=24)  # 日期 +1天
     while True:
         time.sleep(2)
-        socketresult = re.split('=', SOCKETCONTENT)
-        if socketresult[1] == '1':    # 一个检查发送图像成功
-            STUDYINDEX += 1
-            report = reportdic[socketresult[0]]  # 获取报告
-            submitreport(report)                 # 提交报告
-            if STUDYINDEX < len(STUDYLIST):
-                sendimages(STUDYINDEX)  # 发送所有的检查
-            else:                                                            # 一天的检查发完后
-                STUDYINDEX = 0
-                STUDYDONEDATE = STUDYDONEDATE + datetime.timedelta(hours=24)  # 日期 +1天
-                STUDYLIST = getpatientlist(STUDYDONEDATE)  # 获取一条的检查
-        elif socketresult[1] == '0':    # 一个检查发送图像成功
-            pass
-            #保存失败记录
+        print('SOCKETCONTENT %s'% SOCKETCONTENT)
+        if SOCKETCONTENT != '':
+            socketresult = re.split('=', SOCKETCONTENT)
+            if socketresult[1] == '1' or socketresult[1] == '0':    # 接收到socketserver的返回消息:图像上传到云的结果
+                # 保存发送结果到数据库
+                STUDYINDEX += 1
+                if socketresult[1] == '1' and socketresult[0] != '':  #
+                    m1 = hashlib.md5()
+                    m1.update(socketresult[0].encode('utf_8'))
+                    studyuid = m1.hexdigest()  # md5 加密
+                    report = reportdic[studyuid]  # 获取报告
+                    print(report)
+                    time.sleep(3)
+                    submitreport(report)                 # 提交报告
+                if STUDYINDEX < len(STUDYLIST):
+                    SOCKETCONTENT = ''
+                    print(STUDYINDEX)
+                    sendimages()  # 发送一个检查的所有图像
+                else:                                                            # 重新开始一天
+                    STUDYINDEX = 0
+                    print('开始第二天')
+                    if  STUDYDONEDATE < STUDYENDDATE:
+                        STUDYDONEDATE = STUDYDONEDATE + datetime.timedelta(hours=24)  # 日期 +1天
+                        STUDYLIST = getpatientlist()  # 获取一条的检查
+                        reportdic = getreport()  # 获取一天的报告
+                        if STUDYINDEX < len(STUDYLIST):
+                            sendimages()  # 发送一个检查的所有图像
+                        time.sleep(60 * 5)
+                        print(STUDYINDEX)
+                    else:
+                        print('发送完成!')
+                        break
 
-        print(socketresult)
-
-
-
-
+    #
+    #     print(socketresult)
--- report/sendreport.py
+++ report/sendreport.py
@@ -1,12 +1,21 @@
-import urllib.request
-import urllib.parse
+#coding=utf-8
 import hashlib
+import urllib.parse
+import urllib.request
+
+import cx_Oracle
+import mysql.connector
+import mysql.connector
+import datetime
+import time
+import os
 
 
 # 提交报告
-def submitreport(reportdic):
+def submitreport():
     # 接口地址
     url = 'http://app.jdimage.cn/api/report/upload'
+
     # md5加密
     studyuid = ''
     m1 = hashlib.md5()
@@ -36,18 +45,136 @@ def submitreport(reportdic):
     # reportdic['reportDoctorName'] = '吴会权'
     # reportdic['auditDoctorName'] = '孙泽刚'
     # reportdic['reportBody'] = '胸部正侧位'
+    reportdic ={'patientSex': '女', 'officeName': '/', 'pacsCode': 'JDRMYY', 'auditDoctorName': None, 'modality': 'DR', 'patientName': '郑小囡', 'appKey': '', 'reportDoctorName': '张华', 'reportBody': '颈椎(侧位),腰椎正侧位,右肩关节正位,骨盆正位,右手正斜位,右腕正侧位,右尺桡骨正侧位,右肘正侧位,右肱骨正侧位,右足正斜位,右踝正侧位,右胫腓骨正侧位,右膝关节正侧位,胸部正位,右股骨正侧位（中下', 'diagnosis': '颈椎骨质退行性改变，必要时进一步检查。\r\n第4腰椎向前轻度滑移，腰椎退变。\r\n骨盆平片未见异常。\r\n两肺纹理增多，请结合临床。\r\n右侧肩关节脱位伴肱骨大结节撕脱性骨折。\r\n右肱骨中远段未见明显骨折。\r\n右尺桡骨中段骨折。\r\n右腕关节骨质未见明显异常；右手小指、拇指指间关节增生性改变。\r\n右股骨中上段未见明显骨折与异常；右膝关节骨质退行性改变。\r\n右腓骨中下段骨折，右胫骨远段撕脱性骨折可能，右踝关节脱位，建议进一步检查。', 'patientPhone': '/', 'uuid': 'ed09fbbf085cc02a1dfe34fd75a862a7', 'clinicId': '/', 'hospitalName': '建德市第一人民医院', 'imageShow': '   部分颈椎由于体外物重叠影显示欠清，所见颈椎生理曲度变直，部分颈椎体显示唇样骨质增生。各椎间隙未见明显狭窄，附件及小关节无殊。\r\n    第4腰椎向前轻度滑移，诸腰椎边缘唇样增生改变。其余未见明显异常。\r\n    骨盆所组成骨骨皮质连续，骨小梁排列整齐。所见关节未见异常，软组织影清晰。\r\n    两侧胸廓对称，两肺野纹理增多、增粗。两侧肺门结构正常。气管居中。纵隔无增宽。两侧横膈位置形态正常。心脏形态大小正常。两侧肋膈角锐利。所见肋骨未见明显错位骨折征象。\r\n    右肱骨大结节撕脱性骨质断裂，右侧肱骨头下关节盂下移位。右肩诸骨未见明显骨折征象。\r\n    右肱骨中远段骨皮质连续，未见明显骨折征象，所见肘关节间隙无殊。\r\n    右尺桡骨中段双骨折，断端成角错位，周围软组织肿胀。\r\n    右腕关节诸组成骨未见明显骨折征象，关节间隙未见明显异常，周围软组织显示清晰。\r\n    右手诸骨未见明显骨折线，右手小指、拇指指间关节边缘变尖。\r\n    右股骨中上段形态良好，皮质连续，未见明显骨折与异常。关节间隙如常。\r\n    右胫骨髁间隆棘隆起，髌骨上缘骨质变尖、增生，关节间隙未见狭窄，周围软组织未见异常。\r\n    右腓骨中下段骨折，断端移位，胫骨远段局部骨质不连，右踝关节变形。\r\n', 'patientId': 'X290562', 'patientAge': '64', 'applyDoctorName': '/', 'studyDate': '2015-01-06 09:04:31', 'reportDate': '2015-01-06 09:49:06', 'auditDate': '2015-01-06 09:49:07', 'typeName': '/'}
+
     reportdata = urllib.parse.urlencode(reportdic)
     reportdata = reportdata.encode('utf-8')
     with urllib.request.urlopen(url, reportdata) as f:
         postResult = eval(
-            (f.read().decode('utf-8')).replace('true', 'True'))  # {"flag":true}{"msg":"有参数未传","flag":false}
+            (f.read().decode('utf-8')).replace('true', 'True').replace('false',
+                                                                       'False'))  # {"flag":true}{"msg":"有参数未传","flag":false}
         if 'flag' in postResult.keys():
             if True == postResult['flag']:
                 print(postResult['flag'])
             else:  # 发送报告失败，保存到数据库
-                pass
+                print(postResult['flag'])
+                print(postResult['msg'])
         print(f.read().decode('utf-8'))
 
 
+# 查询数据库
+def selSql(selStr):
+    sqlresult = []
+    # print(selStr)
+    connection = cx_Oracle.Connection("radinfo/pacs.jd@192.9.216.21/radinfo")
+    cursor = connection.cursor()
+    try:
+        cursor.execute(selStr)
+    except Exception as e:
+        print(e)
+    sum = 0
+    while 1:
+        row = cursor.fetchone()
+        sum = sum + 1
+        if not row:
+            break
+        sqlresult.append(row)
+        print(row)
+        # print(sum)
+    connection.close()
+    return sqlresult
+
+
+def inserttomysql(startdate, enddate):
+    selstr = 'select PATIENTSALIAS,PATIENTSID,PATIENTSSEX,PATIENTSDOB,STUDIESINSTUID,STUDIESDONEDATE,STUDIESDONETIME,ADMISSIONID,STUDIESMODALITIES,RESULTSBODIESALIAS,RESULTSEXAMINEALIAS,ACCESSIONNUMBER,REPORTSSTATUS,REPORTSDOCTORALIAS,REPORTSDATE,REPORTSTIME,RECORDSDOCTOR,RECORDSDOCTORALIAS,RECORDSDATE,RECORDSTIME,APPROVEDOCTOR,APPROVEDOCTORALIAS,APPROVEDATE,APPROVETIME,REPORTSCONCLUSION,REPORTSEVIDENCES,REPORTSTECHNOLOGIES,REPORTSCOMMENTS  from VHIS_JDYX WHERE   STUDIESDONEDATE>=\'%s\' and STUDIESDONEDATE< \'%s\' order by StudiesDoneTime' % (startdate, enddate)
+    reportlistsql = selSql(selstr)
+    print(reportlistsql[0])
+    # cnx = mysql.connector.connect(user='root', password='',
+    #                               host='127.0.0.1',
+    #                               database='ypacs')
+    # cursor = cnx.cursor()
+    # add_employee = ("INSERT INTO VHIS_JDYX "
+    #                 "(PATIENTSALIAS,PATIENTSID,PATIENTSSEX,PATIENTSDOB,STUDIESINSTUID,STUDIESDONEDATE,STUDIESDONETIME,ADMISSIONID,STUDIESMODALITIES,RESULTSBODIESALIAS,RESULTSEXAMINEALIAS,ACCESSIONNUMBER,REPORTSSTATUS,REPORTSDOCTORALIAS,REPORTSDATE,REPORTSTIME,RECORDSDOCTOR,RECORDSDOCTORALIAS,RECORDSDATE,RECORDSTIME,APPROVEDOCTOR,APPROVEDOCTORALIAS,APPROVEDATE,APPROVETIME,REPORTSCONCLUSION,REPORTSEVIDENCES,REPORTSTECHNOLOGIES,REPORTSCOMMENTS) "
+    #                 "VALUES (%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s) ")
+    #
+    # # insert_new_salary = ("INSERT INTO VHIS_JDYX (PATIENTSALIAS) " "VALUES (%s)")
+    # # print(insert_new_salary)
+    # temp = ('we',)
+    # # Insert new employee
+    # # cursor.execute(insert_new_salary, temp)
+    # for report in reportlistsql:
+    #     cursor.execute(add_employee, report)
+    # # Make sure data is committed to the database
+    #     cnx.commit()
+    # cursor.close()
+    # cnx.close()
+
+def updatemysql(sqlstr, values):
+    # update_old_salary = (
+    #     "UPDATE salaries SET to_date = %s "
+    #     "WHERE emp_no = %s AND from_date = %s")
+    cnx = mysql.connector.connect(user='root', password='',
+                                  host='127.0.0.1',
+                                  database='ypacs')
+    cursor = cnx.cursor()
+    cursor.execute(sqlstr, values)
+    # Make sure data is committed to the database
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+
+
+#
+# def selsqlfrommysql():
+#     # 查询数据库
+#     try:
+#         cnx = mysql.connector.connect(user='root', password='',
+#                                       host='127.0.0.1',
+#                                       database='ypacs')
+#         cursor = cnx.cursor()
+#         query = ("SELECT * FROM VHIS_JDYX ")
+#         cursor.execute(query)
+#         for enddate in cursor:
+#             print(enddate)
+#
+#         cursor.close()
+#         cnx.close()
+#     except mysql.connector.Error as err:
+#         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#             print("Something is wrong with your user name or password")
+#         elif err.errno == errorcode.ER_BAD_DB_ERROR:
+#             print("Database does not exist")
+#         else:
+#             print(err)
+#     else:
+#         cnx.close()
+
+
 if __name__ == "__main__":
+    os.environ['nls_lang'] = 'AMERICAN_AMERICA.AL32UTF8' #解决CX_ORACLE查询出错的编码问题,
     submitreport()
+
+    # selsqlfrommysql()
+    # a = none
+    # print(type(a))
+    # currentdate = datetime.datetime.strptime('20160401', '%Y%m%d')    #发送开始时间
+    # enddate = datetime.datetime.strptime('20160901', '%Y%m%d')  #结束时间
+    # nextdate = currentdate + datetime.timedelta(days=1)
+    #
+    # while 1:
+    #     # time.sleep(1)
+    #     if currentdate < enddate:
+    #         inserttomysql(currentdate.strftime('%Y%m%d'), nextdate.strftime('%Y%m%d'))
+    #         currentdate = nextdate
+    #         nextdate = currentdate + datetime.timedelta(days=1)
+    #         print(currentdate)
+    # sqlstr = (
+    #     "UPDATE vhis_jdyx SET SENDIMAGES = %s "
+    #     "WHERE AccessionNumber = %s ")
+    # values = (1, 'CT163734')
+    # updatemysql(sqlstr, values)
+
+    selstr =' SELECT * FROM VHIS_JDYX  where ACCESSIONNUMBER =\'CT160295\' '
+    selSql(selstr)
+
